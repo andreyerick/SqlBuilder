ec70ffae89d8dd509a2236290e48960ea8f84f79
Andrey Erick <andreyerick1999@gmail.com>
2024-06-30 10:22:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# SqlBuilder
+Minimal secure SQL query builder
+
+## Examples
+```csharp
+var builder = new SqlBuilder("SELECT ExampleEntity.* FROM ExampleEntity %JOIN% %WHERE% %ORDER%");
+
+if (searchModel.Id != null)
+{
+    builder.Where($"ExampleEntity.Id=@id", new SqlParameter("@id", searchModel.Id));
+}
+
+if (searchModel.OtherId != null)
+{
+    builder.InnerJoin("OtherEntity ON OtherEntity.ExampleId=ExampleEntity.Id");
+	builder.Where($"ExampleEntity.OtherId=@otherId", new SqlParameter("@otherId", searchModel.OtherId));
+}
+
+if (!string.IsNullOrWhiteSpace(searchModel.Query))
+{
+    builder.Where($"ExampleEntity.Name LIKE '%' + @name + '%'", new SqlParameter("@name", searchModel.Query));
+    builder.Where($"OR ExampleEntity.Email LIKE '%' + @email + '%'", new SqlParameter("@email", searchModel.Query));
+}
+
+switch (model.Order)
+{
+    default:
+        builder.Order("ExampleEntity.Id DESC");
+        break;
+}
+
+var exampleEntities = _context.ExampleEntities.FromSqlRaw(builder.Sql, builder.SqlParameters).ToList();
+```
+
+## Documentation
+To prevent SQL injection attacks, always pass parameters to the builder instead of concatenating strings, unless you are 100% sure that the string is safe.
+
+### Constructor
+`SqlBuilder(string template)`
+Use `%JOIN%`, `%WHERE%`, `%ORDER%` in your template to replace with joins, where and order by clauses.
+Unused clauses will be removed from the final query.
+Example:
+`var builder = new SqlBuilder("SELECT ExampleEntity.* FROM ExampleEntity %JOIN% %WHERE% %ORDER%");`
+
+### Join
+Use `builder.InnerJoin(string innerJoin)`, `builder.LeftJoin(string leftJoin)` and `builder.RightJoin(string rightJoin)` 
+Example: 
+`builder.InnerJoin("OtherEntity ON OtherEntity.ExampleId=ExampleEntity.Id")`
+
+### Where
+Use `builder.Where(string where, params SqlParameter[] sqlParameters)`
+Example:
+`builder.Where($"Id=@id", new SqlParameter("@id", searchModel.Id))`
+
+### Order
+Use `builder.Order(string order)`
+Example:
+`builder.Order("Id DESC")`
+
+### Sql
+Use `builder.Sql` or `builder.GetSql()` to get the final query string
+
+### SqlParameters
+Use `builder.SqlParameters` or `builder.GetSqlParameters()` to get the final query parameters
+
+### Execute SQL
+Call `DbContext.DbSet<TEntity>.FromSqlRaw()` with `builder.Sql` and `builder.SqlParameters` to execute the query
+Example:
+`var exampleEntities = _context.ExampleEntities.FromSqlRaw(builder.Sql, builder.SqlParameters).ToList();`
---
+++ SqlBuilder.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Data.SqlClient;
+
+public class SqlBuilder
+{
+    public string Sql => GetSql();
+    public SqlParameter[] SqlParameters => GetSqlParameters();
+     
+    protected string _template;
+    protected List<SqlParameter> _parameters;
+
+    protected List<string> _where;
+    protected List<string> _join;
+    protected List<string> _order;
+
+    public SqlBuilder(string template)
+    {
+        _template = template;
+        _parameters = new List<SqlParameter>();
+        _where = new List<string>();
+        _join = new List<string>();
+        _order = new List<string>();
+    }
+
+    public void Where(string where, params SqlParameter[] parameters)
+    {
+        where = where.Trim();
+        if (_where.Count > 0 && !where.ToUpper().StartsWith("AND ") && !where.ToUpper().StartsWith("OR "))
+            where = "AND " + where;
+
+        _where.Add(where);
+
+        if (parameters != null)
+        {
+            _parameters.AddRange(parameters);
+        }
+    }
+
+    public void InnerJoin(string innerJoin)
+    {
+        innerJoin = innerJoin.Trim();
+        if (!innerJoin.ToUpper().StartsWith("INNER JOIN "))
+            innerJoin = "INNER JOIN " + innerJoin;
+
+        _join.Add(innerJoin);
+    }
+
+    public void LeftJoin(string leftJoin)
+    {
+        leftJoin = leftJoin.Trim();
+        if (!leftJoin.ToUpper().StartsWith("LEFT JOIN "))
+            leftJoin = "LEFT JOIN " + leftJoin;
+
+        _join.Add(leftJoin);
+    }
+
+    public void RightJoin(string rightJoin)
+    {
+        rightJoin = rightJoin.Trim();
+        if (!rightJoin.ToUpper().StartsWith("RIGHT JOIN"))
+            rightJoin = "RIGHT JOIN " + rightJoin;
+
+        _join.Add(rightJoin);
+    }
+
+    public void Order(string order)
+    {
+        order = order.Trim();
+        _order.Add(order);
+    }
+
+    public string GetSql()
+    {
+        var sql = _template;
+
+        if (_join.Count > 0)
+        {
+            sql = sql.Replace("%JOIN%", string.Join(" ", _join));
+        }
+        else
+        {
+            sql = sql.Replace("%JOIN%", "");
+        }
+
+        if (_where.Count > 0)
+        {
+            sql = sql.Replace("%WHERE%", "WHERE " + string.Join(" ", _where));
+        }
+        else
+        {
+            sql = sql.Replace("%WHERE%", "");
+        }
+
+        if (_order.Count > 0)
+        {
+            sql = sql.Replace("%ORDER%", "ORDER BY " + string.Join(",", _order));
+        }
+        else
+        {
+            sql = sql.Replace("%ORDER%", "");
+        }
+
+        return sql;
+    }
+
+    public SqlParameter[] GetSqlParameters()
+    {
+        return _parameters.ToArray();
+    }
+}
